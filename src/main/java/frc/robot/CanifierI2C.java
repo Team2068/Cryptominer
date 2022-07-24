24575cceaa4d9d419be269143e6c2d6092e0dc89
Andrew Schineller <schineaj23@pwcs-edu.org>
2022-07-24 02:45:30 +0000

    I may have made things worse.

--- src/main/java/frc/robot/CanifierI2C.java
+++ src/main/java/frc/robot/CanifierI2C.java
@@ -8,21 +8,30 @@ import java.util.concurrent.TimeUnit;
 
 import com.ctre.phoenix.CANifier;
 import com.ctre.phoenix.CANifier.GeneralPin;
+import com.ctre.phoenix.CANifierStatusFrame;
 
 // Implementation based off https://www.ti.com/lit/an/slva704/slva704.pdf
-
 public class CanifierI2C {
     private final CANifier canifier;
 
     private final boolean HIGH = true;
     private final boolean LOW = false; 
 
-    // 5 us delay
-    private final long I2C_DELAY_NS = 5000;
+    private final long I2C_DELAY_NS = TimeUnit.MILLISECONDS.toNanos(10);
     private final long SLEEP_PRECISION = TimeUnit.MILLISECONDS.toNanos(2);
+
+    // Bitmask is LSB -> MSB of GeneralPin index
+    // 0 0 0 0 1 1 0 0 0 0 0
+    //    SCL--^ ^-- SDA
+    private final int SDA_MASK = 0x20;
+    private final int SCL_MASK = 0x40; 
 
     public CanifierI2C(int deviceId) {
         canifier = new CANifier(deviceId);
+
+        // Since the status frame period of the CANifier is 10ms (100Hz)
+        // For Digital Input/Output by default, we can use this instead
+        canifier.setStatusFramePeriod(CANifierStatusFrame.Status_2_General, 10);
     }
 
     private void setSCL(boolean val) {
@@ -41,48 +50,54 @@ public class CanifierI2C {
         return canifier.getGeneralInput(GeneralPin.SDA);
     }
 
-    // sleep function I found on stack overflow, since I'm scared of Thread.sleep()
-    // and its apparent variability.
-    private void delay() throws InterruptedException {
-        final long end = System.nanoTime() + I2C_DELAY_NS;
-        long timeLeft = I2C_DELAY_NS;
+    // Sets SDA/SCL at the same time
+    // It is expensive to call setGeneralOutput twice
+    // If my understanding is correct, though, It should be fine
+    // To call the original methods if setting individually (same time?)
+    private void batchSet(boolean sdaState, boolean sclState) {
+        int mask = 0x0;
+        if(sdaState)
+            mask |= SDA_MASK;
+        if(sclState)
+            mask |= SCL_MASK;
+        canifier.setGeneralOutputs(mask, mask);
+    }
+
+    private void delay() {
+        long start = System.nanoTime();
+        long end = 0;
         do {
-            if (timeLeft > SLEEP_PRECISION)
-                Thread.sleep(1);
-            else
-                Thread.sleep(0);
-            timeLeft = end - System.nanoTime();
-        
-            if(Thread.interrupted())
-                throw new InterruptedException();
-        } while (timeLeft > 0);
+            end = System.nanoTime();
+        } while (start + I2C_DELAY_NS >= end);
     }
 
     // Start sequence: Pull both high for one cycle, then put SDA down.
     private synchronized void startCondition() {
         // Pull both high
-        setSDA(HIGH);
-        setSCL(HIGH);
+        // setSDA(HIGH);
+        // setSCL(HIGH);
 
-        try { delay(); } catch(InterruptedException e) {}
+        batchSet(HIGH, HIGH);
 
-        // After 5 us pull SDA low
+        delay();
+
+        // After 1/2 cycle, pull SDA low
         setSDA(LOW);
 
-        try { delay(); } catch(InterruptedException e) {}
+        delay();
         setSCL(LOW);
     }
 
     // Stop sequence: Keep SCL High and pull up SDA after one cycle.
     private synchronized void stopCondition() {
         setSDA(LOW);
-        try { delay(); } catch(InterruptedException e) {}
+        delay();
         setSCL(HIGH);
         
-        try { delay(); } catch(InterruptedException e) {}
+        delay();
         setSDA(HIGH);
 
-        try { delay(); } catch(InterruptedException e) {}
+        delay();
     }
 
     // Apparently you can only shift ints...
@@ -90,16 +105,21 @@ public class CanifierI2C {
         for(int i=0;i<8;i++) {
             setSDA((data & 0x80) == 1); // Mask for eighth bit
             data = data << 1;
-            try { delay(); } catch(InterruptedException e) {}
+            delay();
             setSCL(HIGH);
-            try { delay(); } catch(InterruptedException e) {}
+            delay();
             setSCL(LOW);
         }
 
         // read that ACK bit!!!!
-        setSDA(HIGH);
-        setSCL(HIGH);
-        try { delay(); } catch(InterruptedException e) {}
+        batchSet(HIGH, HIGH);
+        // setSDA(HIGH);
+        // setSCL(HIGH);
+        delay();
+        
+        // This ideally should be instantaneous
+        // But if my previous assumptions are correct about latency
+        // FIXME: this probably doesn't work
         boolean ack = !getSDA();
         setSCL(LOW);
 
@@ -119,21 +139,25 @@ public class CanifierI2C {
             do {
                 setSCL(HIGH);
             } while(getSCL() == LOW);
-            try { delay(); } catch(InterruptedException e) {}
+            delay();
 
             // Set last bit
             if(getSDA()) data |= 1;
 
-            try { delay(); } catch(InterruptedException e) {}
+            delay();
             setSCL(LOW);
         }
         
-        // ACK bit set to high
-        setSDA(!ack);
-        setSCL(HIGH);
-        try { delay(); } catch(InterruptedException e) {}
-        setSCL(LOW);
-        setSDA(HIGH);
+        // set ACK/NACK
+        batchSet(!ack, HIGH);
+        // setSDA(!ack);
+        // setSCL(HIGH);
+        
+        delay();
+        batchSet(HIGH, LOW);
+        // setSDA(HIGH);
+        // setSCL(LOW);
+        
         return data;
     }
 
@@ -171,6 +195,8 @@ public class CanifierI2C {
         for(int i=0;i<numBytes;i++) {
             data[i] = (i == numBytes-1) ? (byte)readByte(true) : (byte)readByte(false);
         }
+
+        stopCondition();
 
         return true;
     }
